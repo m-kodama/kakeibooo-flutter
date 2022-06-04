33d8c87282d9da3d0a5137e4da190d497e943404
m-kodama <tc003ssr@gmail.com>
2022-06-04 22:05:01 +0900

    feat: Google Fontのリリース用対応
    - ネットワーク経由でDLではなくローカルのアセットから取得するようにした
    - Google Fontのライセンスを含めるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kakeibooo_flutter/feature/home/ui/home_page.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(const MyApp());
 }
 
