b6cbb09fd1b18b8bf01ba514293ff9dcb2487aa5
m-kodama <tc003ssr@gmail.com>
2022-06-05 01:52:55 +0900

    feat: Google Fontsのラインセンスファイルパスが間違っていたため修正

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:kakeibooo_flutter/feature/home/ui/home_page.dart';
 
 void main() {
   LicenseRegistry.addLicense(() async* {
-    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    final license = await rootBundle.loadString('assets/google_fonts/OFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
   });
 
